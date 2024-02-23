bde78023113d211812a1edfee706a12b868f100d
ViniVanC <vincentvan0302@gmail.com>
2024-02-23 10:37:12 +0200

    bugfix
    fix bug loading frame

--- src/js/loading.js
+++ src/js/loading.js
@@ -1,4 +1,4 @@
-if (window.location.pathname === "/loading.html") {
+if (window.location.pathname.endsWith("/loading.html")) {
   setTimeout(function () {
     window.location.href = "choise-pop-up.html";
   }, 3000);
